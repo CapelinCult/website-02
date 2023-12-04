cdf7b05cd8fc9b7ae61a858015d90de9b697547f
CapelinCult <148381943+CapelinCult@users.noreply.github.com>
2023-12-04 00:31:58 -0330

    Add files via upload

---
+++ src/data/profile.ts
@@ -0,0 +1,5 @@
+export const profile = {
+  name: "Justin Smith",
+  title: "Photographer & Designer",
+  bio: "I'm a photographer and designer based in New York City. I specialize in creating beautiful, high-quality photos and designs for my clients.",
+}
---
+++ src/data/settings.ts
@@ -0,0 +1,7 @@
+export const settings = {
+  site: 'https://changeme.com',
+  name: 'Pro File',
+  title: 'Pro File | Template by Little Sticks',
+  description: 'Site description',
+  showLittleSticksPlug: true,
+}
